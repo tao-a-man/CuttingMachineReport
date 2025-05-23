00c8e5697700b4d154f65850756d8e5dce2931c7
WINDOM1\aegon.nguyen <emailaddress@yahoo.com>
2025-05-23 08:08:22 +0700

    v3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Squirrel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,29 @@ namespace CuttingMachineReport
         [STAThread]
         static void Main()
         {
+
+            // Chạy update trước khi mở form
+            Task.Run(async () => await CheckForUpdates()).Wait();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static async Task CheckForUpdates()
+        {
+            try
+            {
+                using (var mgr = new UpdateManager("https://github.com/tao-a-man/CuttingMachineReport/tree/master/Releases"))
+                {
+                    await mgr.UpdateApp();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log hoặc bỏ qua nếu không thể update
+                Console.WriteLine("Update failed: " + ex.Message);
+            }
         }
     }
 }
